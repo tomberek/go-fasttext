f0b5722be337b9924f4db670b06565e2d20d4552
Tom Bereknyei <tom@rebelliondefense.com>
2020-01-17 22:28:42 -0500

    bring cobra work back

---
+++ cmd/wordvec.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/Unknwon/com"
+	fasttext "github.com/clinjie/go-fasttext"
+	"github.com/k0kubun/pp"
+	"github.com/spf13/cobra"
+)
+
+// var (
+// 	unsupervisedModelPath string
+// )
+
+// predictCmd represents the predict command
+var wordvecCmd = &cobra.Command{
+	Use:   "wordvec -m [path_to_model]",
+	Short: "Perform word analogy on a query using an input model",
+	Args:  cobra.ExactArgs(1), // make sure that there is only one argument being passed in
+	Run: func(cmd *cobra.Command, args []string) {
+		if !com.IsFile(unsupervisedModelPath) {
+			fmt.Println("the file %s does not exist", unsupervisedModelPath)
+			return
+		}
+
+		// create a model object
+		model := fasttext.Open(unsupervisedModelPath)
+		// close the model at the end
+		defer model.Close()
+		// perform the prediction
+		wordvec, err := model.Wordvec(args[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		pp.Println(wordvec)
+	},
+}
+
+func init() {
+	wordvecCmd.Flags().StringVarP(&unsupervisedModelPath, "model", "m", "", "path to the fasttext model")
+	rootCmd.AddCommand(wordvecCmd)
+}
